034f17d7b722a88945973c1807917036214772f1
shunichi <khf04440@nifty.com>
2023-07-11 02:52:35 +0900

    add navBar.vue

---
+++ components/atoms/navBar.vue
@@ -0,0 +1,44 @@
+<script>
+import {
+  matHome,
+  matAccountCircle,
+  matMenu,
+} from "@quasar/extras/material-icons";
+export default {
+  setup() {
+    return {
+      matHome,
+      matAccountCircle,
+      matMenu,
+    };
+  },
+};
+</script>
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-bar class="bg-teal text-white">
+      <q-btn dense flat to="/">
+        <div class="q-px-md">
+          <q-icon :name="matHome" />
+          nfa-app
+        </div>
+      </q-btn>
+      <q-space />
+      <div>100%</div>
+      <div class="cursor-pointer non-selectable">
+        <q-icon :name="matMenu" />
+        <q-menu>
+          <q-list dense style="min-width: 100px">
+            <q-item v-close-popup clickable>
+              <q-item-section>Open...</q-item-section>
+            </q-item>
+            <q-item v-close-popup clickable>
+              <q-item-section>New</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </div>
+      <q-icon :name="matAccountCircle" />
+    </q-bar>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <link rel="manifest" href="/manifest.json" />
+    <nav-bar />
     <slot />
   </div>
 </template>
+
+<script>
+import NavBar from "../components/atoms/navBar";
+export default {
+  components: { NavBar },
+};
+</script>
